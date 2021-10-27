2b6f03572efa2c6200a8cbe85abeb00b3efc891b
Martinmeza0 <martinmezacorreo@gmail.com>
2021-10-27 17:06:09 -0300

    bootcamp

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+let notes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      date: '2019-05-30T17:30:31.098Z',
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      date: '2019-05-30T18:39:34.091Z',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      date: '2019-05-30T19:20:14.298Z',
+      important: true
+    }
+  ]
+
+  //node js vanilla
+  // const app = http.createServer((request, response) => {
+  //   response.writeHead(200, { 'Content-Type': 'application/json' })
+  //   response.end(JSON.stringify(notes))
+  // })
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hola<h1/>')
+  })
+
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    //si se encuentra la nota, devuelve la nota (status200)
+    if (note) return response.json(note);
+    //si no, status code 404
+    return response.status(404).end();
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+  })
+
+  app.post('/api/notes/:id', (request, response) =>{
+    const note = request.body
+    console.log(note)
+    response.json(note)
+  })
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
