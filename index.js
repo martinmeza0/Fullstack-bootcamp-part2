e48a82a0f0c4eda3fb8020936bd1344c83df5304
Martinmeza0 <martinmezacorreo@gmail.com>
2021-12-03 09:27:24 -0300

    FIX: error when creating a note, express.json integrated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+
+const cors = require('cors')
 const logger = require('./loggerMiddleware')
 
+app.use(express.json())
 app.use(cors())
 app.use(logger)
  
