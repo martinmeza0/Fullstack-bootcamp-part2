d1a3843f2738561b52f4f8cae1387dadc318fdc8
Martinmeza0 <martinmezacorreo@gmail.com>
2021-12-04 18:48:51 -0300

    Heroku

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use((request, response) => {
     error: 'Not Found'
   })
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
